516405431cf6661be8be5a1e621be57730e59210
sudoalgorithm <me@knrmalhotra.com>
2018-10-06 02:40:15 +0400

    README.md file added for the src submodule. Added flask-rest and gunicorn dependency

---
+++ src/README.md
@@ -0,0 +1,37 @@
+# Steps to run and deploy the application on to IBM Kubernetes Services
+
+## Prerequisites
+
+- Python2.X or Python 3.X
+- Docker
+
+## Testing Locally
+
+### Step - 1: Run the flask app locally
+
+```
+source venv/bin/activate
+```
+```
+pip intall -r requirements.txt
+```
+```
+python app.py
+```
+
+### Step - 2: Build the Docker image
+
+```
+docker build -t flask-web-app:latest
+```
+
+### Step - 3: Build a container from the docker images and run the app
+
+```
+docker run -p -d 5000:5000 flask-web-app
+```
+
+
+
+
+
--- src/app.py
+++ src/app.py
@@ -1,11 +1,16 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, Response ,render_template
+from flask_restful import Resource, Api
 
 app = Flask(__name__)
+api = Api(app)
 
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html')
+class Index(Resource):
+
+    def get(self):
+
+        return Response(render_template('index.html'), mimetype='text/html')
+
+api.add_resource(Index, '/')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)
